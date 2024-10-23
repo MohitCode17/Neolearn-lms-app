653fbbfb7609abd99a4c3eeb2ba00753bd74086c
Mohit Gupta <mohit.codes17@gmail.com>
2024-10-23 14:37:03 +0530

    add cors & default middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,31 @@
 import express from "express";
 import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "./config/env.config.js";
 
 const app = express();
+
+// CORS CONFIG
+app.use(
+  cors({
+    origin: config.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+  })
+);
+
+// DEFAULT MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // SERVER HEALTH ROUTE
 app.get("/", (req, res) => {
--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -4,6 +4,7 @@ conf();
 const _config = {
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
 
 export const config = Object.freeze(_config);
