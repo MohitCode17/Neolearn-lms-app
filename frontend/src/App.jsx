d8f151e7c5575c27ae3803dd12f16525366a136b
Mohit Gupta <mohit.codes17@gmail.com>
2024-10-23 13:48:04 +0530

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold underline mb-2">LMS Mern Stack</h1>
+      <Button>Let's Create</Button>
+    </div>
+  );
+};
+
+export default App;
