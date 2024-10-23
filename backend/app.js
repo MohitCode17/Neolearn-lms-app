3318a75093adf26f50f6c4d2ba0973ab092126c6
Mohit Gupta <mohit.codes17@gmail.com>
2024-10-23 14:31:52 +0530

    add error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const app = express();
 
@@ -6,5 +7,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Server health is okay. ✅");
 });
+
+// ERROR MIDDLEWARE
+app.use(errorMiddleware);
 
 export default app;
