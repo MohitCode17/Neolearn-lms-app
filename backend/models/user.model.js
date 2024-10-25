57386009ed011645a1e01ad2948111ed899ca620
Mohit Gupta <mohit.codes17@gmail.com>
2024-10-25 14:22:09 +0530

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+  },
+});
+
+// HASH PASSWORD
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// COMPARE PASSWORD
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
