92a0db4bb420c62d0496ba9bfe3ea7bba0ce97ad
MohitCode17 <mohit.codes17@gmail.com>
2024-10-27 11:21:17 +0530

    complete login user functionality

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,10 @@
 import CommonForm from "@/components/common-form/form";
 import { loginFormControls } from "@/config";
+import { loginUser } from "@/store/auth-slice";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const initialFormState = {
   email: "",
@@ -10,11 +13,20 @@ const initialFormState = {
 
 const Login = () => {
   const [formData, setFormData] = useState(initialFormState);
+  const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(formData);
+    const data = await dispatch(loginUser(formData));
+
+    if (data?.meta?.requestStatus === "fulfilled") {
+      toast.success(data?.payload?.message || "Login success.");
+    } else if (data?.meta?.requestStatus === "rejected") {
+      toast.error(
+        data?.payload?.message || "Something went wrong, Please try again!"
+      );
+    }
   };
 
   return (
--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -28,6 +28,26 @@ export const registerUser = createAsyncThunk(
   }
 );
 
+// LOGIN USER ASYNC THUNK
+export const loginUser = createAsyncThunk(
+  "/auth/login",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/auth/login",
+        formData,
+        {
+          withCredentials: true,
+        }
+      );
+
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -47,6 +67,24 @@ const authSlice = createSlice({
       .addCase(registerUser.rejected, (state, action) => {
         state.isLoading = false;
         state.isAuthenticated = false;
+        state.user = null;
+        state.error =
+          action.payload?.message || "Something went wrong, Please try again!";
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuthenticated = true;
+        state.user = action.payload?.success ? action.payload?.user : null;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isAuthenticated = false;
+        state.isLoading = false;
         state.user = null;
         state.error =
           action.payload?.message || "Something went wrong, Please try again!";
